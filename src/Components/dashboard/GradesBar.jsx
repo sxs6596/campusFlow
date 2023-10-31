59d029359572309fdc23e008bc7ac98e8dc50827
sxs6596 <sxs6596@mavs.uta.edu>
2023-10-31 18:57:59 -0500

    modified gradesbar.jsx

--- src/Components/dashboard/GradesBar.jsx
+++ src/Components/dashboard/GradesBar.jsx
@@ -1,113 +1,183 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+import axios from 'axios';
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top',
-        },
-        title: {
-            display: true,
-            text: 'Grades Visualization',
-        },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
     },
+    title: {
+      display: true,
+      text: 'Grades Visualization',
+    },
+  },
 };
 
 const gradesData = {
-    labels: ['ML', 'Web Dev', 'SE2', 'UI/UX', 'DataMining'],
-    datasets: [
-        {
-            label: 'Grades',
-            data: [65, 59, 80, 81, 56],
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        }
-    ],
+  labels: [],
+  datasets: [
+    {
+      label: 'Grades',
+      data: [],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+  ],
 };
 
 const studentData = {
-    labels: ['Student1', 'Student2', 'Student3', 'Student4', 'Student5'],
-    datasets: [
-        {
-            label: 'Subject1',
-            data: [90, 92, 87, 95, 88],
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        },
-        {
-            label: 'Subject2',
-            data: [85, 78, 90, 89, 76],
-            backgroundColor: 'rgba(75, 192, 192, 0.5)',
-        },
-        {
-            label: 'Subject3',
-            data: [88, 84, 91, 92, 79],
-            backgroundColor: 'rgba(255, 205, 86, 0.5)',
-        }
-    ],
+  labels: ['Student1', 'Student2', 'Student3', 'Student4', 'Student5'],
+  datasets: [
+    {
+      label: 'Subject1',
+      data: [90, 92, 87, 95, 88],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Subject2',
+      data: [85, 78, 90, 89, 76],
+      backgroundColor: 'rgba(75, 192, 192, 0.5)',
+    },
+    {
+      label: 'Subject3',
+      data: [88, 84, 91, 92, 79],
+      backgroundColor: 'rgba(255, 205, 86, 0.5)',
+    },
+  ],
 };
 
 const buttonStyle = {
-    backgroundColor: "#4CAF50",
-    border: "none",
-    color: "white",
-    padding: "15px 32px",
-    textAlign: "center",
-    textDecoration: "none",
-    display: "inline-block",
-    fontSize: "16px",
-    margin: "4px 2px",
-    cursor: "pointer",
-    transitionDuration: "0.4s",
+  backgroundColor: '#4CAF50',
+  border: 'none',
+  color: 'white',
+  padding: '15px 32px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  display: 'inline-block',
+  fontSize: '16px',
+  margin: '4px 2px',
+  cursor: 'pointer',
+  transitionDuration: '0.4s',
 };
 
 const buttonHoverStyle = {
-    backgroundColor: "white",
-    color: "black",
-    border: "2px solid #4CAF50"
+  backgroundColor: 'white',
+  color: 'black',
+  border: '2px solid #4CAF50',
 };
 
-export default function GradesBar() {
+const GradesBar = () => {
     const [displayData, setDisplayData] = useState(gradesData);
+    const [averageMarks, setAverageMarks] = useState([]);
+    const facultyUserId = parseInt(localStorage.getItem('id'));
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.post('https://rxk4239.uta.cloud/getcourses.php', {
+            faculty_user_id: facultyUserId,
+          });
+          const  response_data = response.data.courses;
+          console.log(response_data);
+          const courses = response.data.courses;
+          const courseIds = courses.map((courseId) => parseInt(courseId));
+  
+          const titlesResponse = await axios.post('https://rxk4239.uta.cloud/getCourseTitles.php', {
+            course_ids: courseIds,
+          });
+          const courseTitles = titlesResponse.data.titles;
+  
+          const averageMarksPromises = courseIds.map(async (courseId) => {
+            console.log('Sending request for course_id:', courseId);  // Log the course_id
+            
+            const response = await axios.post('https://rxk4239.uta.cloud/grades.php', {
+              faculty_user_id: facultyUserId,
+              course_id: courseId,
+            });
+    
+            console.log('Response for course_id:', courseId, response.data);  // Log the response data
+    
+            return response.data.average_percent;
+          });
+          const averageMarks = await Promise.all(averageMarksPromises);
+          setAverageMarks(averageMarks);
+          const colors = [
+            'rgba(255, 99, 132, 0.5)',   // Light Red
+            'rgba(75, 192, 192, 0.5)',   // Cyan
+            'rgba(255, 205, 86, 0.5)',   // Yellow
+            'rgba(54, 162, 235, 0.5)',   // Blue
+            'rgba(153, 102, 255, 0.5)',  // Purple
+            'rgba(255, 159, 64, 0.5)',   // Orange
+            'rgba(255, 99, 204, 0.5)',   // Pink
+            'rgba(139, 195, 74, 0.5)',   // Lime
+            'rgba(240, 98, 146, 0.5)',   // Deep Pink
+            'rgba(121, 85, 72, 0.5)'     // Brown
+          ];
+          
+          // Inside fetchData function:
+          const finalUpdatedGradesData = {
+            labels: courseIds.map((courseId) => courseTitles[courseId]),
+            datasets: [
+              {
+                label: 'Grades',
+                data: averageMarks,
+                backgroundColor: averageMarks.map((_, index) => colors[index % colors.length]),
+              },
+            ],
+          };
+          setDisplayData(finalUpdatedGradesData);
+          
+  
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchData();
+    }, [displayData]);
 
-    const handleToggleClick = () => {
-        setDisplayData((prevData) => 
-            prevData === gradesData ? studentData : gradesData
-        );
-    };
-
-    return (
-        <>
-            <button 
-                style={buttonStyle}
-                onMouseOver={(e) => {
-                    Object.assign(e.currentTarget.style, buttonHoverStyle);
-                }}
-                onMouseOut={(e) => {
-                    Object.assign(e.currentTarget.style, buttonStyle);
-                }}
-                onClick={handleToggleClick}
-            >
-                (Grades/Students)
-            </button>
-            <Bar options={options} data={displayData} />
-        </>
+  const handleToggleClick = () => {
+    setDisplayData((prevData) =>
+      prevData === gradesData ? studentData : gradesData
     );
-}
+  };
+
+  return (
+    <>
+      {/* <button
+        style={buttonStyle}
+        onMouseOver={(e) => {
+          Object.assign(e.currentTarget.style, buttonHoverStyle);
+        }}
+        onMouseOut={(e) => {
+          Object.assign(e.currentTarget.style, buttonStyle);
+        }}
+        onClick={handleToggleClick}
+      >
+        (Grades/Students)
+      </button> */}
+      <Bar options={options} data={displayData} />
+    </>
+  );
+};
+
+export default GradesBar;
--- src/Screens/dashboard/qa/ModifyCourse.jsx
+++ src/Screens/dashboard/qa/ModifyCourse.jsx
@@ -1,61 +1,112 @@
 import { BsFillJournalBookmarkFill } from "react-icons/bs";
-import CourseData from "../../../data/Course"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles/ModifyCourse.css";
+
 export default function ModifyCourse() {
-    const [course, setCourse] = useState(CourseData);
-    function courseModified(e){
+    const [course, setCourse] = useState([]);
+    const [newData, setnewData] = useState({
+            id : 0,
+            title : "",
+            description : "",
+            start_date : "",
+            end_date : "",
+    });
+    useEffect(() => {
+        const getCourses = async () => {
+            const response = await fetch("https://rxk4239.uta.cloud/courses.php");
+            const data = await response.json();
+            // console.log(data.data);
+            setCourse(data.data);
+        };
+        getCourses();
+    }, []);
+
+    function courseModified(e) {
         e.preventDefault();
-        alert("Course Modified")
+        alert("Course Modified");
+        console.log(`updated data is :${newData.id}`);
     }
-    function deleteCourse(e, id){
-        setCourse(course.filter(item=>item.id!==id))
-        alert("Course Deleted")
+
+    function deleteCourse(e, id) {
+        setCourse(course.filter((item) => item.id !== id));
+        alert("Course Deleted");
     }
-    return <>
-        <div className="qa-course-header row">
-            <h2>Update Courses</h2>
-        </div>
-        <form onSubmit={e => courseModified(e)}>
-            <div className="input-core row">
-                <BsFillJournalBookmarkFill className="input-img" />
-                <input className="input" type="text" placeholder="Course Name"  value="CSE 258 001" />
+    const uniqueCourses = course.filter((item, index, self) =>
+  index === self.findIndex((t) => (
+    t.title === item.title
+  ))
+)
+    return (
+        <>
+            <div className="qa-course-header row">
+                <h2>Update Courses</h2>
             </div>
-            <div className="dbl">
+            <form onSubmit={(e) => courseModified(e)}>
                 <div className="input-core row">
-                    <input className="input" type="date" placeholder="Course Name" value="2022-06-01" />
+                    <BsFillJournalBookmarkFill className="input-img" />
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="Enter Course ID"
+                        onChange = {e=>setnewData({...newData,id:e.target.value})}
+                    />
                 </div>
                 <div className="input-core row">
-                    <input className="input" type="date" placeholder="Course Name" value="2023-12-31" />
+                    <BsFillJournalBookmarkFill className="input-img" />
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="Enter Course Name"
+                        onChange = {e=>setnewData({...newData,title:e.target.value})}
+                    />
                 </div>
-            </div>
-            <div className="input-core row">
-                <BsFillJournalBookmarkFill className="input-img" />
-                <input className="input" type="text" placeholder="Course Description" value="Software Engineering. This course covers the principles and practices of software engineering, including
-                        requirements analysis, design, testing, and maintenance. Students will learn how to work in teams to develop software
-                        projects using modern software engineering tools and techniques." />
-            </div>
-            <button className="button">
-                Update Courses
-            </button>
-        </form>
-        <div>
-           {
-            course.map(item=>{
-                return <>
-                    <div className="course-box row">
-                        <div className="course-box-left">
-                            <h3>{item.title}</h3>
-                        </div>
-                        <div className="course-box-right">
-                            <button className="deleteBtn" onClick={e => deleteCourse(e,item.id)}>
-                                Delete
-                            </button>
-                        </div>
+                <div className="dbl">
+                    <div className="input-core row">
+                        <input
+                            className="input"
+                            type="date"
+                            placeholder="start date"
+                            onChange = {e=>setnewData({...newData,start_date:e.target.value})}
+                        />
                     </div>
-                </>
-            })
-           }
-        </div>
-    </>
+                    <div className="input-core row">
+                        <input
+                            className="input"
+                            type="date"
+                            placeholder="end date"
+                            onChange = {e=>setnewData({...newData,end_date:e.target.value})}
+                        />
+                    </div>
+                </div>
+                <div className="input-core row">
+                    <BsFillJournalBookmarkFill className="input-img" />
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="Course Description"
+                        onChange = {e=>setnewData({...newData,description:e.target.value})}
+                    />
+                </div>
+                <button className="button">Update Courses</button>
+            </form>
+            <div>
+                {
+                    uniqueCourses.map(item=>{
+                        return <>
+                            <div className="course-box row">
+                                <div className="course-box-left">
+                                    <h3>{item.title}</h3>
+                                </div>
+                                <div className="course-box-right">
+                                    <button className="deleteBtn" onClick={e => deleteCourse(e,item.id)}>
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+                        </>
+                    })
+                }
+            </div>
+        </>
+    );
 }
