6c320a6cafa7248f4058fa81b3bf583c1aabb02c
sxs6596 <sxs6596@mavs.uta.edu>
2023-10-31 17:05:31 -0500

    modified studentListScreen

--- src/Screens/dashboard/StudentListScreen.jsx
+++ src/Screens/dashboard/StudentListScreen.jsx
@@ -1,31 +1,36 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios"; // Import Axios
+
 import StudentCard from "../../Components/dashboard/StudentCard";
+
 export default function StudentListScreen() {
   const facultyUserId = parseInt(localStorage.getItem('id'));
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    // Prepare the data to be sent in the POST request
-    const postData = { faculty_user_id: facultyUserId };
-    console.log("**",postData)
-    console.log("*",students)
+    // Create an async function to make the POST request
+    async function fetchData() {
+      try {
+        // Prepare the data to be sent in the POST request
+        const postData = { faculty_user_id: facultyUserId };
 
-    // Make a POST request to the PHP endpoint to send the students state
-    fetch("https://rxk4239.uta.cloud/getStudents.php", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(postData), // Send the postData as JSON
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setStudents(data);
-        // console.log(data);
-      })
-      .catch((error) => {
+        // Make a POST request to the PHP endpoint using Axios with async/await
+        const response = await axios.post("https://rxk4239.uta.cloud/getStudents.php", postData, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        // Set the students state with the response data
+        setStudents(response.data);
+        console.log(response.data);
+      } catch (error) {
         console.error("Error fetching student data: ", error);
-      });
+      }
+    }
+
+    // Call the async fetchData function
+    fetchData();
   }, [facultyUserId]); // Include 'facultyUserId' in the dependency array
 
   return (
