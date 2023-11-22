c12a0732bd6db0648cf3af0b03e48fa0afc73647
sxs6596 <sxs6596@mavs.uta.edu>
2023-11-22 17:47:06 -0600

    modified course card layout

--- src/Screens/dashboard/students/Course.jsx
+++ src/Screens/dashboard/students/Course.jsx
@@ -2,6 +2,7 @@ import "./styles/Course.css"
 import { useEffect, useState } from "react"
 import CourseList from "../../../data/Course"
 import CourseCard from "../../../Components/dashboard/CourseCard"
+import {Container, Grid} from '@radix-ui/themes';
 import axios from 'axios'; 
 export default function Course() {
     const [data, setData] = useState([]);
@@ -23,12 +24,21 @@ export default function Course() {
         <div className="course-header">
             <h2>All Courses</h2>
         </div>
-        <div className="course-container row">
+        {/* <div className="course-container row">
         {
             data.map((course) => {
                 return <CourseCard key={course.id} {...course} />
             })
         }
-        </div>
+        </div> */}
+        <Container>
+        <Grid columns="2" gap="5" width="auto">
+        {
+            data.map((course) => {
+                return <CourseCard key={course.id} {...course} />
+            })
+        }
+        </Grid>
+        </Container>
     </>
 }
